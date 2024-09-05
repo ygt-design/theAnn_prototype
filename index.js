ffc924791cbcc0f7ae17d484fc9137d7492b0cf6
Yiğit Toprak <103063122+ygt-design@users.noreply.github.com>
2024-09-05 11:45:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import LocomotiveScroll from "./node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js";
-
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
